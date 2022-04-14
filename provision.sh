4d4029e1f528de9b0486adbee60a6335ab55cbf1
jihoonkim <j5kim@ucsd.edu>
2022-04-13 22:07:15 -0700

    updat gensim

---
+++ provision.sh
@@ -0,0 +1,18 @@
+conda create --name nlp python=3.9
+
+conda activate nlp
+
+
+conda install -y -c conda-forge jupyterlab
+conda install -y -c anaconda openpyxl
+conda install -y -c anaconda gensim
+conda install -y -c anaconda pandas
+conda install -y -c anaconda nltk
+conda install -y -c anaconda scikit-learn
+
+
+conda install -y -c conda-forge gensim
+conda install -y -c anaconda seaborn
+
+conda install -c conda-forge spacy
+conda install -c conda-forge pyldavis
